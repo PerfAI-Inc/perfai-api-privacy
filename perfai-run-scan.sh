aec439ba16f51bbba0783bf45d3f3ed16f441a8d
Mohd Rashid <59992299+mohdrashid1@users.noreply.github.com>
2024-06-08 14:10:01 +0530

    Update perfai-run-scan.sh

--- perfai-run-scan.sh
+++ perfai-run-scan.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Begin
 
-TEMP=$(getopt -n "$0" -a -l "username:,password:,apiSpecURL:,apiBasePath:,authUrl:,authBody:,authHeaders:,licenseKey:,label:,governance_email:" -- -- "$@")
+
+TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,openapi_spec:,governance_email:,version:,name:,source:,url:,client_id:,client_secret:,audience:,grant_type:" -- -- "$@")
+# TEMP=$(getopt -n "$0" -a -l "username:,password:,apiSpecURL:,apiBasePath:,authUrl:,authBody:,authHeaders:,licenseKey:,label:,governance_email:" -- -- "$@")
 # TEMP=$(getopt -n "$0" -a -l "base_url:,auth0_url:,register_url:,username:,password:,openapi_spec:,base_path:,label:,governance_email:" -- -- "$@")
 
     [ $? -eq 0 ] || exit
@@ -11,9 +13,19 @@ TEMP=$(getopt -n "$0" -a -l "username:,password:,apiSpecURL:,apiBasePath:,authUr
     while [ $# -gt 0 ]
     do
              case "$1" in
+                    --hostname) PERFAI_HOSTNAME="$2"; shift;;
                     --username) PERFAI_USER="$2"; shift;;
                     --password) PERFAI_PWD="$2"; shift;;
-                    --apiSpecURL) API_SPEC_URL="$2"; shift;;
+                    --openapi_spec) OPENAPI_SPEC="$2"; shift;;
+                    --governance_email) GOVERNANCE_EMAIL="$2"; shift;;
+                    --name) NAME="$2"; shift;;
+                    --source) SOURCE="$2"; shift;;
+                    --version) VERSION="$2"; shift;;
+                    --url) URL="$2"; shift;;
+                    --client_id) CLIENT_ID="$2"; shift;;
+                    --client_secret) CLIENT_SECRET="$2"; shift;;
+                    --audience) AUDIENCE="$2"; shift;;
+                    --grant_type) GRANT_TYPE="$2"; shift;;                    
                     # --authBody) AUTH_BODY="$2"; shift;;
                     # --authUrl) AUTH_URL="$2"; shift;;
                     # --authHeaders) AUTH_HEADERS="$2"; shift;;
@@ -31,16 +43,18 @@ AUTH0_URL=${AUTH0_URL:-"https://dev-y3450b42cwy8vyl1.us.auth0.com"}
 REGISTER_URL=${REGISTER_URL:-"https://app.apicontract.dev/apis/register"}
 
 
-if [ "$BASE_URL" = "" ]
+if [ "$PERFAI_HOSTNAME" = "" ];
 then
-   BASE_URL="https://app.apicontract.dev"
-fi    
+PERFAI_HOSTNAME="https://api.perfai.ai"
+fi
 
 echo " "
-token=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${PERFAI_USER}"'", "password": "'"${PERFAI_PWD}"'"}' "${AUTH_URL}"/login | jq -r .token)
+token=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${PERFAI_USER}"'", "password": "'"${PERFAI_PWD}"'"}' "${PERFAI_HOSTNAME}"/login)
 echo "generated token is:" "$token"
 echo " "
 
+curl -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "${PERFAI_HOSTNAME}/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $token" -d  '{"openapi_spec":"'"${OPENAPI_SPEC}"'","source":"'"${SOURCE}"'","version":"'"${VERSION}"'","name":"'"${NAME}"'","governance_email":"'"${GOVERNANCE_EMAIL}"'","auth0_url":"'"${AUTH0_URL}"'","register_url":"'"${REGISTER_URL}"'"}'
 #DATA=$(curl -k -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "https://api.dev.perfai.ai/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $token" -d  '{"base_url":"'"${BASE_URL}"'","auth0_url":"'"${AUTH0_URL}"'","register_url":"'"${REGISTER_URL}"'","openapi_spec":"'"${OPENAPI_SPEC}"'","base_path":"'"${BASEPATH_SPEC}"'","label":"'"${LABEL}"'","governance_email":"'"${GOVERNANCE_EMAIL}"'"}' | jq -r .token)
-curl -k -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "https://api.perfai.ai/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $token" -d  '{"apiSpecURL":"'"${API_SPEC_URL}"'","authUrl":"'"${AUTH_URL}"'","authBody":"'"${AUTH_BODY}"'","authHeaders":"'"${AUTH_HEADERS}"'","licenseKey":"'"${LICENSE_KEY}"'","label":"'"${LABEL}"'","email_reports":"'"${EMAIL_REPORTS}"'"}'
+#curl -k -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "https://api.perfai.ai/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $token" -d  '{"apiSpecURL":"'"${API_SPEC_URL}"'","authUrl":"'"${AUTH_URL}"'","authBody":"'"${AUTH_BODY}"'","authHeaders":"'"${AUTH_HEADERS}"'","licenseKey":"'"${LICENSE_KEY}"'","label":"'"${LABEL}"'","email_reports":"'"${EMAIL_REPORTS}"'"}'
 
+echo "Successfully created the API Register."
