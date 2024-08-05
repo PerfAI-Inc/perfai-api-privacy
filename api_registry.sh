9b1d682b2d3c857f0eed66d2b76150ec57aeb268
Mohd Rashid <59992299+mohdrashid1@users.noreply.github.com>
2024-08-05 11:00:40 +0530

    Update and rename m2m.sh to api_registry.sh

--- api_registry.sh
+++ api_registry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,api_endpoint:,openapi_spec:,governance_email:,version:,name:,source:" -- -- "$@")
+TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,openapi_spec:,governance_email:,version:,name:,source:" -- -- "$@")
 
      [ $? -eq 0 ] || exit
 
@@ -12,7 +12,6 @@ TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,api_endpoint:,openapi
                --hostname) PERFAI_HOSTNAME="$2"; shift;;
                --username) PERFAI_USERNAME="$2"; shift;;
                --password) PERFAI_PASSWORD="$2"; shift;;
-               --api_endpoint) API_ENDPOINT="$2"; shift;;
                --openapi_spec) OPENAPI_SPEC="$2"; shift;;
                --governance_email) GOVERNANCE_EMAIL="$2"; shift;;
                --name) NAME="$2"; shift;;
@@ -22,14 +21,6 @@ TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,api_endpoint:,openapi
           esac
           shift;
      done
-
-### THIS IS FOR API REGISTRY ###
-if [ "$API_ENDPOINT" = "" ];
-then
-API_ENDPOINT="https://api.perfai.ai"
-fi
-
-echo " "
 
 TOKEN_RESPONSE=$(curl -s --location --request POST "$API_ENDPOINT/api/v1/auth/token" \
 --header "Content-Type: application/json" \
@@ -43,6 +34,6 @@ ACCESS_TOKEN=$(echo $TOKEN_RESPONSE | jq -r '.id_token')
 echo "Access Token is: $ACCESS_TOKEN"
 echo " "
 
-API_REGISTRY_RESPONSE=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "$API_ENDPOINT/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $ACCESS_TOKEN" -d "{\"openapi_spec\":\"${OPENAPI_SPEC}\",\"source\":\"${SOURCE}\",\"version\":\"${VERSION}\",\"name\":\"${NAME}\",\"governance_email\":\"${GOVERNANCE_EMAIL}\"}")
+API_REGISTRY_RESPONSE=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "https://api.perfai.ai/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $ACCESS_TOKEN" -d "{\"openapi_spec\":\"${OPENAPI_SPEC}\",\"source\":\"${SOURCE}\",\"version\":\"${VERSION}\",\"name\":\"${NAME}\",\"governance_email\":\"${GOVERNANCE_EMAIL}\"}")
 
 echo "API Registry Successfully: $API_REGISTRY_RESPONSE"
