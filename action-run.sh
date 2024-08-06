532838bc3d27f644921718ae8b079b1d3aaecd25
Mohd Rashid <59992299+mohdrashid1@users.noreply.github.com>
2024-08-06 17:54:19 +0530

    Update action-run.sh

--- action-run.sh
+++ action-run.sh
@@ -2,25 +2,27 @@
 
 TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,openapi_spec:,governance_email:,version:,name:,source:" -- -- "$@")
 
-     [ $? -eq 0 ] || exit
+[ $? -eq 0 ] || exit
 
-     eval set -- "$TEMP"
+eval set -- "$TEMP"
 
-     while [ $# -gt 0 ] 
-     do
-          case "$1" in
-               --hostname) PERFAI_HOSTNAME="$2"; shift;;
-               --username) PERFAI_USERNAME="$2"; shift;;
-               --password) PERFAI_PASSWORD="$2"; shift;;
-               --openapi_spec) OPENAPI_SPEC="$2"; shift;;
-               --governance_email) GOVERNANCE_EMAIL="$2"; shift;;
-               --name) NAME="$2"; shift;;
-               --source) SOURCE="$2"; shift;;
-               --version) VERSION="$2"; shift;;
-               --) shift ;;
-          esac
-          shift;
-     done
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --hostname) PERFAI_HOSTNAME="$2"; shift;;
+        --username) PERFAI_USERNAME="$2"; shift;;
+        --password) PERFAI_PASSWORD="$2"; shift;;
+        --openapi_spec) OPENAPI_SPEC="$2"; shift;;
+        --governance_email) GOVERNANCE_EMAIL="$2"; shift;;
+        --name) NAME="$2"; shift;;
+        --source) SOURCE="$2"; shift;;
+        --version) VERSION="$2"; shift;;
+        --) shift ;;
+    esac
+    shift;
+done
+
+echo " "
 
 ### Step 1: Print Access Token ###
 TOKEN_RESPONSE=$(curl -s --location --request POST "https://api.perfai.ai/api/v1/auth/token" \
@@ -32,5 +34,39 @@ TOKEN_RESPONSE=$(curl -s --location --request POST "https://api.perfai.ai/api/v1
 
 ACCESS_TOKEN=$(echo $TOKEN_RESPONSE | jq -r '.id_token')
 
+if [ -z "$ACCESS_TOKEN" ]; then
+    echo "Failed to retrieve access token."
+    echo "Response was: $TOKEN_RESPONSE"
+    exit 1
+fi
+
 echo "Access Token is: $ACCESS_TOKEN"
 echo " "
+
+
+### Step 2: Retrieve Catalog ID ###
+CATALOG_RESPONSE=$(curl -s --location --request GET "https://api.perfai.ai/api/v1/sensitive-data-service/apps/all?page=1&pageSize=1" \
+--header "Authorization: Bearer $ACCESS_TOKEN")
+
+CATALOG_ID=$(echo $CATALOG_RESPONSE | jq -r '.data[0].catalog_id')
+
+if [ -z "$CATALOG_ID" ]; then
+    echo "Failed to retrieve catalog ID."
+    echo "Response was: $CATALOG_RESPONSE"
+    exit 1
+fi
+
+echo "Catalog ID is: $CATALOG_ID"
+echo " "
+
+
+### Step 3: Schedule Action-Run ###
+RUN_RESPONSE=$(curl -s --location --request POST "https://api.perfai.ai/api/v1/api-catalog/apps/schedule-run-multiple" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $ACCESS_TOKEN" \
+--data "{
+    \"catalog_id\": \"${CATALOG_ID}\",
+    \"services\": [\"governance\", \"vms\", \"sensitive\", \"apitest\", \"performance\"]
+}")
+
+echo "Run Response: $RUN_RESPONSE"
