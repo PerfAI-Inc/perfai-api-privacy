c05e990fffa626b0702ddd3f4bd5c9577dd34ad3
Mohd Rashid <59992299+mohdrashid1@users.noreply.github.com>
2024-09-06 19:13:29 +0530

    Update api_registry.sh

--- api_registry.sh
+++ api_registry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,openapi_spec:,governance_email:,version:,label:,source:" -- -- "$@")
+TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,openapi_spec:,governance_email:,version:,label:,base_path:,source:" -- -- "$@")
 
      [ $? -eq 0 ] || exit
 
@@ -17,6 +17,7 @@ TEMP=$(getopt -n "$0" -a -l "hostname:,username:,password:,openapi_spec:,governa
                --label) LABEL="$2"; shift;;
                --source) SOURCE="$2"; shift;;
                --version) VERSION="$2"; shift;;
+               --base_path) BASE_PATH="$2"; shift;;
                --) shift ;;
           esac
           shift;
@@ -36,6 +37,6 @@ echo "Access Token is: $ACCESS_TOKEN"
 echo " "
 
 # Step 2: Extract the access token from the response and make the API request
-API_REGISTRY_RESPONSE=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "https://api.perfai.ai/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $ACCESS_TOKEN" -d "{\"openapi_spec\":\"${OPENAPI_SPEC}\",\"source\":\"${SOURCE}\",\"version\":\"${VERSION}\",\"label\":\"${LABEL}\",\"governance_email\":\"${GOVERNANCE_EMAIL}\"}")
+API_REGISTRY_RESPONSE=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" --location --request POST "https://api.perfai.ai/api/v1/api-catalog/apps/create-run" --header "Authorization: Bearer $ACCESS_TOKEN" -d "{\"openapi_spec\":\"${OPENAPI_SPEC}\",\"source\":\"${SOURCE}\",\"base_path\":\"${BASE_PATH}\",\"version\":\"${VERSION}\",\"label\":\"${LABEL}\",\"governance_email\":\"${GOVERNANCE_EMAIL}\"}")
 
 echo "API Registry Response: $API_REGISTRY_RESPONSE"
